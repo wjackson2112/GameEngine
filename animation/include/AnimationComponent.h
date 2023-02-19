232641d826ffbc0f0f14ab1ab8848424f65f1297
wjackson2112 <wjackson2112@gmail.com>
2023-02-19 12:20:51 -0500

    Support check for all anims complete in component

--- animation/include/AnimationComponent.h
+++ animation/include/AnimationComponent.h
@@ -16,6 +16,7 @@ public:
     void addAndStart(Animation* newAnimation);
     void skipAll();
     void update(float deltaTime);
+    bool hasAnimations() { return !animations.empty(); }
 
     template<class T>
     std::vector<T> getAnimations()
